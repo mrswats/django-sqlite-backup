1b1addd34fa6ba709120484480fddd525143fbc4
Ferran Jovell <ferran@jovell.dev>
2023-04-20 11:56:41 +0200

    Final stuff

--- django_sqlite_backup/aws.py
+++ django_sqlite_backup/aws.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-from pathlib import Path
 from typing import Any
-from typing import Union
 
 import boto3
 from django.conf import ImproperlyConfigured
@@ -11,25 +9,25 @@ from django.conf import settings
 class AwsSqliteBackup:
     def __init__(self) -> None:
         self.s3 = boto3.client("s3")
-        self._db_name: Union[str, None] = None
 
-    def get_database_name(self) -> Union[str, Path]:
-        if self._db_name is None:
-            self._db_name = settings.DATABASES["default"]["NAME"]
-
-        return self._db_name
+    def get_database_name(self) -> str:
+        return str(settings.DATABASES["default"]["NAME"])
 
     def _read_db(self) -> Any:
         with open(self.get_database_name()) as f:
-            return f
+            return f.read()
 
     def backup_db(self) -> None:
         bucket_name = settings.SQLITE_BACKUP.get("BUCKET_NAME")
-        db_name = self.get_database_name()
+        db_name = self.get_database_name().split("/")[-1]
 
         if bucket_name is None:
             raise ImproperlyConfigured("`BUCKET_NAME` is not defined")
 
-        full_bucket_name = f"{bucket_name}/{datetime.now().strftime('%Y-%m-%d')}/{db_name}"
+        full_bucket_name = f"sqlite_backup/{datetime.now().strftime('%Y-%m-%d')}/{db_name}"
 
-        self.s3.put_object(Key=full_bucket_name, Body=self._read_db())
+        self.s3.put_object(
+            Bucket=bucket_name,
+            Key=full_bucket_name,
+            Body=self._read_db(),
+        )
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from django.test import Client
 
@@ -18,3 +20,10 @@ def unauthenticated_client():
 def default_settings(settings):
     settings.SQLITE_BACKUP = {}
     return settings
+
+
+@pytest.fixture
+def aws_credentials():
+    os.environ["AWS_ACCESS_KEY_ID"] = "my-id"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "my-secret"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
